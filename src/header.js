3eccfb6e497a9ee590d1f5c589f8ec1c819b9773
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-22 18:07:23 +0200

    Create header section

--- src/header.js
+++ src/header.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import leaf from './img/leaf.png';
 
 class Header extends React.Component{
     render(){
         return(
-            
+            <header>
+                <div className="container">
+                    <div className="logo">
+                        <span>
+                            <img src={leaf} alt="leaf icon"/>
+                        </span>
+                        <p>
+                            Treehouse
+                        </p>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Blog
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Portfolio
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <i className="fas fa-bars"></i>
+                </div>
+            </header>
         )
     }
 }
+
+export default Header;
