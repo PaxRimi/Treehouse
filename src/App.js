63c9fba7a03f2366e7c7c55bfb91234bcdf19254
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-27 14:26:37 +0200

    Add portfolio section to the main file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './header';
+import Portfolio from './portfolio';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Portfolio/>
       </div>
     );
   }
