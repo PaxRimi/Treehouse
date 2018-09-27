84da8c795e32c704cebe8a228604eaa4ba471e2e
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-27 14:26:06 +0200

    Create portfolio section

---
+++ src/portfolio.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Portfolio extends React.Component{
+    render(){
+        return(
+            <section className="portfolio">
+                <div className="container">
+                    <h1>
+                        Creative Digital Solutions
+                    </h1>
+                    <p>
+                        Proin iaculis purus consequat sem cure.
+                    </p>
+                    <button>
+                        View Portfolio
+                    </button>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Portfolio;
